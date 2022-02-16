6d7d07c4f59da9432d8ebe399e4186e44bdbd52b
Agrey Srivastava <agreysrivastava@gmail.com>
2022-02-17 00:38:04 +0530

    1.1 AvgTime

---
+++ AvgTime.py
@@ -0,0 +1,35 @@
+from random import Random, random
+import time
+
+def current_time_millis():
+    return round(time.time()*1000)
+
+before = current_time_millis()
+a = 0
+for i in range(1,1000000):
+    a += i
+print(a)
+after = current_time_millis()
+print("Addition")
+print(after-before)
+
+#Multplication logic
+import random
+b=1
+before = current_time_millis()
+for i in range(1,1000): 
+    b *= random.randint(1,10)
+print(b)
+after = current_time_millis()
+print("Multplication")
+print(after - before)
+
+#Division Logic
+before = current_time_millis()
+for i in range(1,1000000): 
+    c = random.randint(1,1000)/2
+print(c)
+after = current_time_millis()
+print("Division")
+print(after - before)
+
