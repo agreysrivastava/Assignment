aa63ce02e0b404134a90839dd874983184c61f69
Agrey Srivastava <agreysrivastava@gmail.com>
2022-02-19 03:38:11 +0530

    Assignmen 2.2

---
+++ Gauss Siedel.py
@@ -0,0 +1,34 @@
+def take_array(n,m):
+    arr = []
+    for i in range(0,n):
+        row = []
+        for j in range(0,m):
+            row.append(int(input()))
+        arr.append(row)
+    return arr
+
+#Input array
+size = int(input("Enter Size: "))
+dominant_diagonal_array = take_array(size,size)
+value_array = take_array(size,1)
+
+#Iteration Matrix
+iteration_matrix = []
+init_row = [0]*size
+rowno = 0
+no_exit = True
+while no_exit:
+    no_exit = False
+    for i in range(size):
+        sum = 0
+        for j in range(size):
+            if i != j:
+                sum += dominant_diagonal_array[i][j]*init_row[j]
+        init_row[i] = (value_array[i][0] - sum)/dominant_diagonal_array[i][i]
+    iteration_matrix.append(init_row)
+    for x in range(size):
+        if rowno == 0 or (init_row[x] - iteration_matrix[rowno-1][x]) > 0.001:
+            no_exit = True
+    rowno += 1
+
+print(iteration_matrix)
