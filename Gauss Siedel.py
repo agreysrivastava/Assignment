b1207d076bee1a814aab0d9cf651e8bd54d6229e
Agrey Srivastava <agreysrivastava@gmail.com>
2022-02-20 10:00:04 +0530

    Fixes in Gauss Siedel

--- Gauss Siedel.py
+++ Gauss Siedel.py
@@ -13,11 +13,13 @@ dominant_diagonal_array = take_array(size,size)
 value_array = take_array(size,1)
 
 #Iteration Matrix
+from copy import deepcopy
 iteration_matrix = []
 init_row = [0]*size
 rowno = 0
 no_exit = True
 while no_exit:
+    iteration_matrix.append([0]*size)
     no_exit = False
     for i in range(size):
         sum = 0
@@ -25,10 +27,11 @@ while no_exit:
             if i != j:
                 sum += dominant_diagonal_array[i][j]*init_row[j]
         init_row[i] = (value_array[i][0] - sum)/dominant_diagonal_array[i][i]
-    iteration_matrix.append(init_row)
+    iteration_matrix[rowno] = deepcopy(init_row)
     for x in range(size):
         if rowno == 0 or (init_row[x] - iteration_matrix[rowno-1][x]) > 0.001:
             no_exit = True
+            break
     rowno += 1
 
 print(iteration_matrix)
